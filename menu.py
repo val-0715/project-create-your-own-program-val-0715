70db6fcd73200376a49a5f4958f07070736264e2
val-0715 <valeriem97@nycstudents.net>
2026-04-14 17:24:20 +0000

    done

--- menu.py
+++ menu.py
@@ -1,15 +1,23 @@
 print("Welcome to the Flashcard CLI!",
 "\n1. Create a flashcard",
 "\n2. Review flashcards",
-"\n3. Exit",
-"\nPlease select an option (1, 2, or 3)")
+"\n3. Delete Flashcards",
+"\n4. Exit",
+"\nPlease select an option (1, 2, 3, or 4)")
 input_option = input("Option: ")
 
 if input_option == "1":
     import storage
+    from storage import create_flashcards
+    create_flashcards()
 elif input_option == "2":
     import flashcards_cli
 elif input_option == "3":
+    import storage
+    from storage import delete_flashcards
+    delete_flashcards()
+
+elif input_option == "4":
     print("Goodbye!")
 else:
     print("Invalid option, please try again.")
--- storage.py
+++ storage.py
@@ -1,31 +1,61 @@
 import json
-import classes 
-from classes import ques_var, ans_var
-def collect_input():
 
+def reading_data():
     try:
-        with open('flashinfo.json', 'r') as jsonfiles:
-            data = json.load(jsonfiles)
-            new_flashcard = data + lists
+        with open('flashinfo.json', 'r') as f:
+            data = json.load(f)
+            return data
+
     except FileNotFoundError:
+        data = [{
+        "Questions":"pratice",
+        "Answers":"flashcard" ,
+        "ID": 0
+        }]
+
         with open('flashinfo.json', 'w') as f:
-            json.dump(new_flashcard, f, indent = 2)
-            print("success!")
+            json.dump(data, f, indent = 2)
+
+def flashcards():
+    import classes 
+    from classes import ques_var, ans_var
+
+
+    with open('flashinfo.json', 'r') as f:
+            data_converter = json.load(f)
+            num = data_converter[-1]["ID"] if data_converter else 0
+            num += 1
+            
+
+            
+    flashcards = {
+        "Questions": ques_var,
+        "Answers": ans_var,
+        "ID": num
+    }
+    lists = ([flashcards])
+    
+    new_flashcard = data_converter + lists
+
+    return new_flashcard
+
+def create_flashcards():
+    flash_var = flashcards()
     with open('flashinfo.json', 'w') as f:
-            json.dump(new_flashcard, f, indent = 2)
+            json.dump(flash_var, f, indent = 2)
             print("success!")
 
-#with open('flashinfo.json', 'r') as jsonfiles:
- #       data = jsonfiles.read()
-  #      data_converter = json.loads(data)
-   #     id = data_converter[-1]["ID"] if data_converter else 0
-    #    id += 1
-id = 2
+def delete_flashcards():
+    d = reading_data()
+    usr_input = int(input("Choose ID number to delete flashcard: "))
+    data = [card for card in d if card.get('ID') != usr_input]
 
-flashcards = {
-    "Questions": ques_var,
-    "Answers": ans_var,
-    "ID": id
-}
-lists = ([flashcards])
-collect_input()
+    with open('flashinfo.json', "w") as f:
+        json.dump(data, f, indent = 2)
+    
+
+    
+reading_data()
+#flashcards()
+#create_flashcards()
+#delete_flashcards()
